0284e299a31a06c6b54f61a838d7935ad30835d6
Arata <arata04962@gmail.com>
2024-03-07 01:03:18 +0900

    feat: TraceLayerを追加した

--- crates/sos24-presentation/src/route.rs
+++ crates/sos24-presentation/src/route.rs
@@ -4,6 +4,9 @@ use axum::{
     routing::{delete, get, post, put},
     Router,
 };
+use tower::ServiceBuilder;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 
 use crate::module::Modules;
 
@@ -22,4 +25,12 @@ pub fn create_app(modules: Modules) -> Router {
         .route("/health", get(health::handle_get))
         .nest("/news", news)
         .with_state(Arc::new(modules))
+        .layer(
+            ServiceBuilder::new().layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                    .on_request(DefaultOnRequest::new().level(Level::INFO))
+                    .on_response(DefaultOnResponse::new().level(Level::INFO)),
+            ),
+        )
 }
