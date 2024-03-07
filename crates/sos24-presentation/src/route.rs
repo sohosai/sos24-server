27d9174afeaae84f3fd003e1231fabcf15f9e493
Arata <arata04962@gmail.com>
2024-03-08 04:52:35 +0900

    fix: ルーティングが誤っていたのを修正した

--- crates/sos24-presentation/src/route.rs
+++ crates/sos24-presentation/src/route.rs
@@ -31,7 +31,7 @@ pub fn create_app(modules: Modules) -> Router {
 
     let user = Router::new()
         .route("/", get(user::handle_get))
-        .route("/", get(user::handle_post))
+        .route("/", post(user::handle_post))
         .route("/:user_id", get(user::handle_get_id))
         .route("/:user_id", delete(user::handle_delete_id))
         .route("/:user_id", put(user::handle_put_id));
