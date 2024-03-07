bd73d5f34d59bec6a73119c37970e4a01eacfe5f
Arata <arata04962@gmail.com>
2024-03-08 04:41:31 +0900

    feat: userのエンドポイントを実装した

--- crates/sos24-presentation/src/model/user.rs
+++ crates/sos24-presentation/src/model/user.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
-use sos24_use_case::dto::user::{CreateUserDto, UserCategoryDto};
+use sos24_use_case::dto::user::{
+    CreateUserDto, UpdateUserDto, UserCategoryDto, UserDto, UserRoleDto,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreateUser {
@@ -25,6 +27,97 @@ impl From<CreateUser> for CreateUserDto {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateUser {
+    pub name: String,
+    pub kana_name: String,
+    pub email: String,
+    pub password: String,
+    pub phone_number: String,
+    pub role: UserRole,
+    pub category: UserCategory,
+}
+
+pub trait ConvertToUpdateUserDto {
+    fn to_update_user_dto(self) -> UpdateUserDto;
+}
+
+impl ConvertToUpdateUserDto for (String, UpdateUser) {
+    fn to_update_user_dto(self) -> UpdateUserDto {
+        let (id, user) = self;
+        UpdateUserDto::new(
+            id,
+            user.name,
+            user.kana_name,
+            user.email,
+            user.phone_number,
+            UserRoleDto::from(user.role),
+            UserCategoryDto::from(user.category),
+        )
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: String,
+    pub name: String,
+    pub kana_name: String,
+    pub email: String,
+    pub phone_number: String,
+    pub role: UserRole,
+    pub category: UserCategory,
+    pub created_at: String,
+    pub updated_at: String,
+    pub deleted_at: Option<String>,
+}
+
+impl From<UserDto> for User {
+    fn from(value: UserDto) -> Self {
+        User {
+            id: value.id,
+            name: value.name,
+            kana_name: value.kana_name,
+            email: value.email,
+            phone_number: value.phone_number,
+            role: value.role.into(),
+            category: value.category.into(),
+            created_at: value.created_at.to_rfc3339(),
+            updated_at: value.updated_at.to_rfc3339(),
+            deleted_at: value.deleted_at.map(|it| it.to_rfc3339()),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum UserRole {
+    Administrator,
+    CommitteeOperator,
+    Committee,
+    General,
+}
+
+impl From<UserRole> for UserRoleDto {
+    fn from(value: UserRole) -> Self {
+        match value {
+            UserRole::Administrator => UserRoleDto::Administrator,
+            UserRole::CommitteeOperator => UserRoleDto::CommitteeOperator,
+            UserRole::Committee => UserRoleDto::Committee,
+            UserRole::General => UserRoleDto::General,
+        }
+    }
+}
+
+impl From<UserRoleDto> for UserRole {
+    fn from(value: UserRoleDto) -> Self {
+        match value {
+            UserRoleDto::Administrator => UserRole::Administrator,
+            UserRoleDto::CommitteeOperator => UserRole::CommitteeOperator,
+            UserRoleDto::Committee => UserRole::Committee,
+            UserRoleDto::General => UserRole::General,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub enum UserCategory {
     UndergraduateStudent,
     GraduateStudent,
@@ -37,6 +130,16 @@ impl From<UserCategory> for UserCategoryDto {
             UserCategory::UndergraduateStudent => UserCategoryDto::UndergraduateStudent,
             UserCategory::GraduateStudent => UserCategoryDto::GraduateStudent,
             UserCategory::AcademicStaff => UserCategoryDto::AcademicStaff,
+        }
+    }
+}
+
+impl From<UserCategoryDto> for UserCategory {
+    fn from(value: UserCategoryDto) -> Self {
+        match value {
+            UserCategoryDto::UndergraduateStudent => UserCategory::UndergraduateStudent,
+            UserCategoryDto::GraduateStudent => UserCategory::GraduateStudent,
+            UserCategoryDto::AcademicStaff => UserCategory::AcademicStaff,
         }
     }
 }
--- crates/sos24-presentation/src/route/user.rs
+++ crates/sos24-presentation/src/route/user.rs
@@ -1,12 +1,20 @@
 use std::sync::Arc;
 
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
 use sos24_use_case::{
     dto::user::CreateUserDto,
     error::{user::UserError, UseCaseError},
 };
 
-use crate::{model::user::CreateUser, module::Modules};
+use crate::{
+    model::user::{ConvertToUpdateUserDto, CreateUser, UpdateUser, User},
+    module::Modules,
+};
 
 use super::ToStatusCode;
 
@@ -20,6 +28,21 @@ impl ToStatusCode for UseCaseError<UserError> {
     }
 }
 
+pub async fn handle_get(
+    State(modules): State<Arc<Modules>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let raw_user_list = modules.user_use_case().list().await;
+    raw_user_list
+        .map(|raw_user_list| {
+            let user_list: Vec<User> = raw_user_list.into_iter().map(User::from).collect();
+            (StatusCode::OK, Json(user_list))
+        })
+        .map_err(|err| {
+            tracing::error!("Failed to list user: {err}");
+            err.status_code()
+        })
+}
+
 pub async fn handle_post(
     State(modules): State<Arc<Modules>>,
     Json(raw_user): Json<CreateUser>,
@@ -28,6 +51,44 @@ pub async fn handle_post(
     let res = modules.user_use_case().create(user).await;
     res.map(|_| StatusCode::CREATED).map_err(|err| {
         tracing::error!("Failed to create user: {err}");
+        err.status_code()
+    })
+}
+
+pub async fn handle_get_id(
+    Path(id): Path<String>,
+    State(modules): State<Arc<Modules>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let raw_user = modules.user_use_case().find_by_id(id).await;
+    match raw_user {
+        Ok(raw_user) => Ok((StatusCode::OK, Json(User::from(raw_user)))),
+        Err(err) => {
+            tracing::error!("Failed to find user: {err}");
+            Err(err.status_code())
+        }
+    }
+}
+
+pub async fn handle_delete_id(
+    Path(id): Path<String>,
+    State(modules): State<Arc<Modules>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let res = modules.user_use_case().delete_by_id(id).await;
+    res.map(|_| StatusCode::OK).map_err(|err| {
+        tracing::error!("Failed to delete user: {err}");
+        err.status_code()
+    })
+}
+
+pub async fn handle_put_id(
+    Path(id): Path<String>,
+    State(modules): State<Arc<Modules>>,
+    Json(raw_user): Json<UpdateUser>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let user = (id, raw_user).to_update_user_dto();
+    let res = modules.user_use_case().update(user).await;
+    res.map(|_| StatusCode::OK).map_err(|err| {
+        tracing::error!("Failed to update user: {err}");
         err.status_code()
     })
 }
