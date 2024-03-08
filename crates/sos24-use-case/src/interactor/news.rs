4f7551204105738192c8f3c7e806f3e885193235
Arata <arata04962@gmail.com>
2024-03-08 18:09:50 +0900

    feat: newsの認可周りを実装した

--- crates/sos24-use-case/src/interactor/news.rs
+++ crates/sos24-use-case/src/interactor/news.rs
@@ -2,7 +2,12 @@ use std::sync::Arc;
 
 use anyhow::Context;
 use sos24_domain::{
-    entity::news::{NewsBody, NewsCategories, NewsId, NewsTitle},
+    ensure,
+    entity::{
+        actor::Actor,
+        news::{NewsBody, NewsCategories, NewsId, NewsTitle},
+        permission::Permissions,
+    },
     repository::{news::NewsRepository, Repositories},
 };
 
@@ -27,8 +32,9 @@ impl<R: Repositories> NewsUseCase<R> {
         Self { repositories }
     }
 
-    pub async fn list(&self) -> Result<Vec<NewsDto>, NewsError> {
-        // TODO: 権限チェック
+    pub async fn list(&self, actor: &Actor) -> Result<Vec<NewsDto>, NewsError> {
+        ensure!(actor.has_permission(Permissions::READ_NEWS_ALL));
+
         let raw_news_list = self
             .repositories
             .news_repository()
@@ -43,8 +49,9 @@ impl<R: Repositories> NewsUseCase<R> {
         Ok(news_list)
     }
 
-    pub async fn create(&self, raw_news: CreateNewsDto) -> Result<(), NewsError> {
-        // TODO: 権限チェック
+    pub async fn create(&self, actor: &Actor, raw_news: CreateNewsDto) -> Result<(), NewsError> {
+        ensure!(actor.has_permission(Permissions::CREATE_NEWS));
+
         let news = raw_news.into_entity()?;
         self.repositories
             .news_repository()
@@ -54,8 +61,9 @@ impl<R: Repositories> NewsUseCase<R> {
         Ok(())
     }
 
-    pub async fn find_by_id(&self, id: String) -> Result<NewsDto, NewsError> {
-        // TODO: 権限チェック
+    pub async fn find_by_id(&self, actor: &Actor, id: String) -> Result<NewsDto, NewsError> {
+        ensure!(actor.has_permission(Permissions::READ_NEWS_ALL));
+
         let id: NewsId = id.try_into()?;
         let raw_news = self
             .repositories
@@ -70,8 +78,9 @@ impl<R: Repositories> NewsUseCase<R> {
         }
     }
 
-    pub async fn update(&self, news_data: UpdateNewsDto) -> Result<(), NewsError> {
-        // TODO: 権限チェック
+    pub async fn update(&self, actor: &Actor, news_data: UpdateNewsDto) -> Result<(), NewsError> {
+        ensure!(actor.has_permission(Permissions::UPDATE_NEWS_ALL));
+
         let id: NewsId = news_data.id.try_into().context("Failed to parse news id")?;
         let news = self
             .repositories
@@ -82,9 +91,9 @@ impl<R: Repositories> NewsUseCase<R> {
             .ok_or(UseCaseError::UseCase(NewsError::NotFound(id)))?;
 
         let mut new_news = news.value;
-        new_news.set_title(NewsTitle::new(news_data.title));
-        new_news.set_body(NewsBody::new(news_data.body));
-        new_news.set_categories(NewsCategories::new(news_data.categories));
+        new_news.set_title(actor, NewsTitle::new(news_data.title))?;
+        new_news.set_body(actor, NewsBody::new(news_data.body))?;
+        new_news.set_categories(actor, NewsCategories::new(news_data.categories))?;
 
         self.repositories
             .news_repository()
@@ -95,8 +104,9 @@ impl<R: Repositories> NewsUseCase<R> {
         Ok(())
     }
 
-    pub async fn delete_by_id(&self, id: String) -> Result<(), NewsError> {
-        // TODO: 権限チェック
+    pub async fn delete_by_id(&self, actor: &Actor, id: String) -> Result<(), NewsError> {
+        ensure!(actor.has_permission(Permissions::DELETE_NEWS_ALL));
+
         let id: NewsId = id.try_into().context("Failed to parse news id")?;
 
         self.repositories
