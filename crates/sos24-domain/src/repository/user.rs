2e42af0be1cf2e67ad26199a39e152affc81bd05
Arata <arata04962@gmail.com>
2024-03-09 07:53:14 +0900

    refactor: user repositoryのエラー型を定義した

--- crates/sos24-domain/src/repository/user.rs
+++ crates/sos24-domain/src/repository/user.rs
@@ -1,24 +1,35 @@
 use std::future::Future;
 
 use mockall::automock;
+use thiserror::Error;
 
 use crate::entity::{
     common::date::WithDate,
     user::{User, UserId},
 };
 
+#[derive(Debug, Error)]
+pub enum UserRepositoryError {
+    #[error(transparent)]
+    InternalError(#[from] anyhow::Error),
+}
+
 #[automock]
 pub trait UserRepository: Send + Sync + 'static {
-    fn list(&self) -> impl Future<Output = anyhow::Result<Vec<WithDate<User>>>> + Send;
+    fn list(&self)
+        -> impl Future<Output = Result<Vec<WithDate<User>>, UserRepositoryError>> + Send;
 
-    fn create(&self, user: User) -> impl Future<Output = anyhow::Result<()>> + Send;
+    fn create(&self, user: User) -> impl Future<Output = Result<(), UserRepositoryError>> + Send;
 
     fn find_by_id(
         &self,
         id: UserId,
-    ) -> impl Future<Output = anyhow::Result<Option<WithDate<User>>>> + Send;
+    ) -> impl Future<Output = Result<Option<WithDate<User>>, UserRepositoryError>> + Send;
 
-    fn update(&self, user: User) -> impl Future<Output = anyhow::Result<()>> + Send;
+    fn update(&self, user: User) -> impl Future<Output = Result<(), UserRepositoryError>> + Send;
 
-    fn delete_by_id(&self, id: UserId) -> impl Future<Output = anyhow::Result<()>> + Send;
+    fn delete_by_id(
+        &self,
+        id: UserId,
+    ) -> impl Future<Output = Result<(), UserRepositoryError>> + Send;
 }
