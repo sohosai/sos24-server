658c5476829c682a7fc6658f1f0e956a84032a50
Arata <arata04962@gmail.com>
2024-03-09 12:07:25 +0900

    test: newsのテストを実装した

--- crates/sos24-use-case/src/interactor/news.rs
+++ crates/sos24-use-case/src/interactor/news.rs
@@ -113,3 +113,205 @@ impl<R: Repositories> NewsUseCase<R> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use sos24_domain::{
+        entity::{permission::PermissionDeniedError, user::UserRole},
+        test::{fixture, repository::MockRepositories},
+    };
+
+    use crate::{
+        dto::news::{CreateNewsDto, UpdateNewsDto},
+        interactor::news::{NewsUseCase, NewsUseCaseError},
+    };
+
+    #[tokio::test]
+    async fn list_general_success() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_list()
+            .returning(|| Ok(vec![fixture::date::with(fixture::news::news1())]));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::General);
+        let res = use_case.list(&actor).await;
+        assert!(matches!(res, Ok(_)));
+    }
+
+    #[tokio::test]
+    async fn create_committee_fail() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_create()
+            .returning(|_| Ok(()));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::Committee);
+        let res = use_case
+            .create(
+                &actor,
+                CreateNewsDto::new(
+                    fixture::news::title1().value(),
+                    fixture::news::body1().value(),
+                    fixture::news::categories1().value(),
+                ),
+            )
+            .await;
+        assert!(matches!(
+            res,
+            Err(NewsUseCaseError::PermissionDeniedError(
+                PermissionDeniedError
+            ))
+        ));
+    }
+
+    #[tokio::test]
+    async fn create_operator_success() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_create()
+            .returning(|_| Ok(()));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::CommitteeOperator);
+        let res = use_case
+            .create(
+                &actor,
+                CreateNewsDto::new(
+                    fixture::news::title1().value(),
+                    fixture::news::body1().value(),
+                    fixture::news::categories1().value(),
+                ),
+            )
+            .await;
+        assert!(matches!(res, Ok(_)));
+    }
+
+    #[tokio::test]
+    async fn find_by_id_general_success() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_find_by_id()
+            .returning(|_| Ok(Some(fixture::date::with(fixture::news::news1()))));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::General);
+        let res = use_case
+            .find_by_id(&actor, fixture::news::id1().value().to_string())
+            .await;
+        assert!(matches!(res, Ok(_)));
+    }
+
+    #[tokio::test]
+    async fn update_committee_fail() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_find_by_id()
+            .returning(|_| Ok(Some(fixture::date::with(fixture::news::news1()))));
+        repositories
+            .news_repository_mut()
+            .expect_update()
+            .returning(|_| Ok(()));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::Committee);
+        let res = use_case
+            .update(
+                &actor,
+                UpdateNewsDto::new(
+                    fixture::news::id1().value().to_string(),
+                    fixture::news::title2().value(),
+                    fixture::news::body2().value(),
+                    fixture::news::categories2().value(),
+                ),
+            )
+            .await;
+        assert!(matches!(
+            res,
+            Err(NewsUseCaseError::PermissionDeniedError(
+                PermissionDeniedError
+            ))
+        ));
+    }
+
+    #[tokio::test]
+    async fn update_operator_success() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_find_by_id()
+            .returning(|_| Ok(Some(fixture::date::with(fixture::news::news1()))));
+        repositories
+            .news_repository_mut()
+            .expect_update()
+            .returning(|_| Ok(()));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::CommitteeOperator);
+        let res = use_case
+            .update(
+                &actor,
+                UpdateNewsDto::new(
+                    fixture::news::id1().value().to_string(),
+                    fixture::news::title2().value(),
+                    fixture::news::body2().value(),
+                    fixture::news::categories2().value(),
+                ),
+            )
+            .await;
+        assert!(matches!(res, Ok(_)));
+    }
+
+    #[tokio::test]
+    async fn delete_by_id_committee_fail() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_find_by_id()
+            .returning(|_| Ok(Some(fixture::date::with(fixture::news::news1()))));
+        repositories
+            .news_repository_mut()
+            .expect_delete_by_id()
+            .returning(|_| Ok(()));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::Committee);
+        let res = use_case
+            .delete_by_id(&actor, fixture::news::id1().value().to_string())
+            .await;
+        assert!(matches!(
+            res,
+            Err(NewsUseCaseError::PermissionDeniedError(
+                PermissionDeniedError
+            ))
+        ));
+    }
+
+    #[tokio::test]
+    async fn delete_by_id_operator_success() {
+        let mut repositories = MockRepositories::default();
+        repositories
+            .news_repository_mut()
+            .expect_find_by_id()
+            .returning(|_| Ok(Some(fixture::date::with(fixture::news::news1()))));
+        repositories
+            .news_repository_mut()
+            .expect_delete_by_id()
+            .returning(|_| Ok(()));
+        let use_case = NewsUseCase::new(Arc::new(repositories));
+
+        let actor = fixture::actor::actor1(UserRole::CommitteeOperator);
+        let res = use_case
+            .delete_by_id(&actor, fixture::news::id1().value().to_string())
+            .await;
+        assert!(matches!(res, Ok(_)));
+    }
+}
