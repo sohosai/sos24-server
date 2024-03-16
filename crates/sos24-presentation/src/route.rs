3fffc84acb84f3569df0ade8f5812f86f0f6607a
Arata <arata04962@gmail.com>
2024-03-17 08:55:14 +0900

    feat: /projects/meを実装した

--- crates/sos24-presentation/src/route.rs
+++ crates/sos24-presentation/src/route.rs
@@ -36,6 +36,7 @@ pub fn create_app(modules: Modules) -> Router {
     let project = Router::new()
         .route("/", get(project::handle_get))
         .route("/", post(project::handle_post))
+        .route("/me", get(project::handle_get_me))
         .route("/:project_id", get(project::handle_get_id))
         .route("/:project_id", delete(project::handle_delete_id))
         .route("/:project_id", put(project::handle_put_id));
