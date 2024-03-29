0979806b28f3853082adfd7eacc73e9b0fc88df5
Arata <arata04962@gmail.com>
2024-03-30 03:30:52 +0900

    fix: typo

--- crates/sos24-presentation/src/error/convert_error.rs
+++ crates/sos24-presentation/src/error/convert_error.rs
@@ -1,15 +1,5 @@
 use axum::http::StatusCode;
 
-use sos24_domain::entity::common::datetime::DateTimeError;
-use sos24_domain::entity::file_data::FileIdError;
-use sos24_domain::entity::form::{FormError, FormIdError, FormItemIdError};
-use sos24_domain::entity::form_answer::FormAnswerIdError;
-use sos24_domain::entity::project::BoundedStringError;
-use sos24_domain::repository::file_data::FileDataRepositoryError;
-use sos24_domain::repository::file_object::FileObjectRepositoryError;
-use sos24_domain::repository::form::FormRepositoryError;
-use sos24_domain::repository::form_answer::FormAnswerRepositoryError;
-use sos24_domain::service::verify_form_answer::VerifyFormAnswerError;
 use sos24_domain::{
     entity::{
         common::email::EmailError,
@@ -23,9 +13,16 @@ use sos24_domain::{
         news::NewsRepositoryError, project::ProjectRepositoryError, user::UserRepositoryError,
     },
 };
-use sos24_use_case::interactor::file::FileUseCaseError;
-use sos24_use_case::interactor::form::FormUseCaseError;
-use sos24_use_case::interactor::form_answer::FormAnswerUseCaseError;
+use sos24_domain::entity::common::datetime::DateTimeError;
+use sos24_domain::entity::file_data::FileIdError;
+use sos24_domain::entity::form::{FormError, FormIdError, FormItemIdError};
+use sos24_domain::entity::form_answer::FormAnswerIdError;
+use sos24_domain::entity::project::BoundedStringError;
+use sos24_domain::repository::file_data::FileDataRepositoryError;
+use sos24_domain::repository::file_object::FileObjectRepositoryError;
+use sos24_domain::repository::form::FormRepositoryError;
+use sos24_domain::repository::form_answer::FormAnswerRepositoryError;
+use sos24_domain::service::verify_form_answer::VerifyFormAnswerError;
 use sos24_use_case::{
     context::ContextError,
     interactor::{
@@ -33,6 +30,9 @@ use sos24_use_case::{
         user::UserUseCaseError,
     },
 };
+use sos24_use_case::interactor::file::FileUseCaseError;
+use sos24_use_case::interactor::form::FormUseCaseError;
+use sos24_use_case::interactor::form_answer::FormAnswerUseCaseError;
 
 use super::AppError;
 
@@ -234,7 +234,7 @@ impl From<ProjectUseCaseError> for AppError {
             ),
             ProjectUseCaseError::AlreadyOwnedProject(_) => AppError::new(
                 StatusCode::CONFLICT,
-                "project/alread-owned-project".to_string(),
+                "project/already-owned-project".to_string(),
                 message,
             ),
             ProjectUseCaseError::ApplicationsNotAccepted => AppError::new(
