4a0123a53c880580065cff3909e39ffa8e37d202
Arata <arata04962@gmail.com>
2024-03-29 19:11:40 +0900

    fix: 申請回答時に制約を満たしているか確認するように

--- crates/sos24-use-case/src/interactor/form_answer/update.rs
+++ crates/sos24-use-case/src/interactor/form_answer/update.rs
@@ -20,6 +20,13 @@ impl<R: Repositories> FormAnswerUseCase<R> {
         form_answer_data: UpdateFormAnswerDto,
     ) -> Result<(), FormAnswerUseCaseError> {
         let actor = ctx.actor(Arc::clone(&self.repositories)).await?;
+        let owned_project_id = ctx
+            .project(Arc::clone(&self.repositories))
+            .await?
+            .map(|project| match project {
+                OwnedProject::Owner(project) => project.value.id().clone(),
+                OwnedProject::SubOwner(project) => project.value.id().clone(),
+            });
 
         let id = FormAnswerId::try_from(form_answer_data.id)?;
         let form_answer = self
@@ -29,18 +36,27 @@ impl<R: Repositories> FormAnswerUseCase<R> {
             .await?
             .ok_or(FormAnswerUseCaseError::NotFound(id))?;
 
-        ensure!(form_answer.value.is_updatable_by(&actor));
+        ensure!(form_answer
+            .value
+            .is_updatable_by(&actor, owned_project_id.clone()));
 
         let mut new_form_answer = form_answer.value;
-        new_form_answer
-            .set_project_id(&actor, ProjectId::try_from(form_answer_data.project_id)?)?;
-        new_form_answer.set_form_id(&actor, FormId::try_from(form_answer_data.form_id)?)?;
         let new_items = form_answer_data
             .items
             .into_iter()
             .map(|item| item.into_entity())
             .collect::<Result<_, _>>()?;
-        new_form_answer.set_items(&actor, new_items)?;
+        new_form_answer.set_items(&actor, owned_project_id, new_items)?;
+
+        let form_id = new_form_answer.form_id().clone();
+        let form = self
+            .repositories
+            .form_repository()
+            .find_by_id(form_id.clone())
+            .await?
+            .ok_or(FormAnswerUseCaseError::FormNotFound(form_id))?;
+
+        verify_form_answer::verify(&form.value, &new_form_answer)?;
 
         self.repositories
             .form_answer_repository()
