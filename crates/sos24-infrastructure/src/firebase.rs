565a0afdcbb60481fc22f1adc96bfad55537afbf
Arata <arata04962@gmail.com>
2024-04-12 01:35:51 +0900

    feat: ログ出力を追加

--- crates/sos24-infrastructure/src/firebase.rs
+++ crates/sos24-infrastructure/src/firebase.rs
@@ -8,11 +8,15 @@ pub struct FirebaseAuth(LiveAuthAdmin);
 
 impl FirebaseAuth {
     pub async fn new(service_account_key: &str) -> anyhow::Result<Self> {
+        tracing::info!("Initializing Firebase Auth");
+
         let gcp_service_account = CustomServiceAccount::from_json(service_account_key)?;
         let live_app = App::live(gcp_service_account.into())
             .await
             .map_err(|err| anyhow::anyhow!("{}", err))?;
         let live_auth_app = live_app.auth();
+
+        tracing::info!("Firebase Auth initialized");
         Ok(Self(live_auth_app))
     }
 }
