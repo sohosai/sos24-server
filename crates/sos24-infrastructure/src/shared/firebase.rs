026e6005bc7b8013e5cb2b2d2a0d6541173e2406
Arata <arata04962@gmail.com>
2025-02-08 03:00:42 +0900

    fix: rs-firebase-admin-sdkの更新に対応

--- crates/sos24-infrastructure/src/shared/firebase.rs
+++ crates/sos24-infrastructure/src/shared/firebase.rs
@@ -1,15 +1,22 @@
 use std::ops::Deref;
 
-use rs_firebase_admin_sdk::{App, CustomServiceAccount, LiveAuthAdmin};
+use rs_firebase_admin_sdk::{credentials_provider, App, LiveAuthAdmin};
 
 pub struct FirebaseAuth(LiveAuthAdmin);
+
+const ENV_KEY: &str = "GOOGLE_APPLICATION_CREDENTIALS";
 
 impl FirebaseAuth {
     pub async fn new(service_account_key: &str) -> anyhow::Result<Self> {
         tracing::info!("Initializing Firebase Auth");
 
-        let gcp_service_account = CustomServiceAccount::from_json(service_account_key)?;
-        let live_app = App::live(gcp_service_account.into())
+        // credentials_provider()は環境変数からクレデンシャルを取得するので、一時的に環境変数を設定する
+        // ref: https://docs.rs/gcp_auth/0.12.3/gcp_auth/fn.provider.html
+        std::env::set_var(ENV_KEY, service_account_key);
+        let gcp_service_account = credentials_provider().await?;
+        std::env::remove_var(ENV_KEY);
+
+        let live_app = App::live(gcp_service_account)
             .await
             .map_err(|err| anyhow::anyhow!("{}", err))?;
         let live_auth_app = live_app.auth();
